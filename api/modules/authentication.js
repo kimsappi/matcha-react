35d86ed4560246d60d3b138223c760c3a7dcc0ed
Kim Sappi <ksappi@student.hive.fi>
2020-07-31 15:21:00 +0300

    Authenticated requests are now refused on all browsers after user has logged out in one browser

--- api/modules/authentication.js
+++ api/modules/authentication.js
@@ -1,20 +1,36 @@
 const jwt = require('jsonwebtoken');
+const mysql = require('mysql');
 
 const {tokenSecret} = require('../config.json');
+const pool = require('./dbConnect');
 
 const authenticationMiddleware = (req, res, next) => {
 	const authHeader = req.headers['authorization'];
 	const token = authHeader && authHeader.split(' ')[1];
-	if (!token)
+	if (!token) {
 		req.user = null;
-	else
+		next();
+	}
+	else {
 		jwt.verify(token, tokenSecret, (err, user) => {
-			if (err)
+			if (err) {
 				req.user = null;
-			else
-				req.user = user;
+				next();
+			}
+			else {
+				console.log('querying online status');
+				pool.query(mysql.format('SELECT `online` FROM users WHERE id = ?;', [user.id]), (error, results) => {
+				//pool.query('SELECT `online` FROM users WHERE id = 1;', (error, results) => {
+					if (error || !results[0].online)
+						return res.json('logged out');
+					else {
+						req.user = user;
+						next();
+					}
+				})
+			}
 		});
-	next();
+	}
 }
 
 const generateJWT = data => jwt.sign(data, tokenSecret);
