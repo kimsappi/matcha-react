02467efac921cdab185d0f412c40c0e022e546b3
Kim Sappi <ksappi@student.hive.fi>
2020-08-04 16:18:13 +0300

    Moved websocket initialisation out of bin/www

---
+++ api/websocket.js
@@ -0,0 +1,17 @@
+const socket = require('socket.io');
+
+const wsServerInit = server => {
+	const io = socket(server);
+
+	io.on('connection', function(socket) {
+		socket.on('chat', function(jees) {
+		  io.emit('chat', jees);
+		  console.log("asd");
+		  console.log(jees);
+		})
+		console.log("jees");
+		console.log(socket.id);
+	});
+};
+
+module.exports = wsServerInit;
