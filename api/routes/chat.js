883fbb64f69356b5cfacc042830e38f7812f04e1
Kim Sappi <ksappi@student.hive.fi>
2020-08-05 12:23:00 +0300

    Back-end now returns all previous chat messages

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -2,10 +2,16 @@ const mysql = require('mysql');
 const pool = require('../modules/dbConnect');
 
 const get = (req, res, next) => {
-	return res.json(null);
-	if (!req.user)
+	if (!req.user || (!req.query.id && !req.query.id !== '0'))
 		return res.json(null);
-	const query = 'SELECT * FROM messages WHERE '
+	const query = 'SELECT * FROM chat_and_user WHERE (sender_id = ? OR recipient_id = ?) AND (id > ?);';
+	console.log(mysql.format(query, [req.user.id, req.user.id, parseInt(req.query.id)]));
+	pool.query(mysql.format(query, [req.user.id, req.user.id, parseInt(req.query.id)]), (error, results) => {
+		if (error)
+			return res.json(null);
+		else
+			return res.json(results);
+	});
 };
 
 const post = (req, res, next) => {
