503dae3a8e27343797492290bccae3cd06798801
Kim Sappi <ksappi@student.hive.fi>
2020-08-14 12:38:08 +0300

    Location setting is now done through a map

---
+++ client/src/views/MyProfile/MyProfileMap.js
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import {apiKey} from '../../apiKey.json';
+
+const containerStyle = {
+	width: '400px',
+	height: '400px'
+};
+
+const MyProfileMap = ({lat, lon, setLat, setLon}) => {
+	const [mapCentre, setMapCentre] = useState({lat: 0, lng: 0});
+
+	useEffect(() => {
+		setMapCentre({lat: parseFloat(lat), lng: parseFloat(lon)});
+	}, []);
+
+	const dragEndHandler = (event) => {
+		setLat(event.latLng.lat());
+		setLon(event.latLng.lng());
+	};
+
+	return (
+		<LoadScript
+			googleMapsApiKey={apiKey}
+		>
+			<GoogleMap
+				mapContainerStyle={containerStyle}
+				center={mapCentre}
+				zoom={6}
+			>
+				<Marker
+					draggable={true}
+					position={mapCentre}
+					onDragEnd={event => dragEndHandler(event)}
+				/>
+			</GoogleMap>
+		</LoadScript>
+	)
+};
+
+export default MyProfileMap;
